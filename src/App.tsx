40650cdb0fdd3edf752ac1ecd68abb2e76f59d42
Youngkwang Shin <sykkkkk412@naver.com>
2021-09-14 00:42:26 +0900

    commit 2: 퀵스타트 및  api테스트

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Seeso from 'seeso/dist/seeso'
+import { UserStatusOption } from 'seeso';
+import { version } from 'process';
 
-function App() {
+const App: React.FC = () => {
+  // 영광, seeso개발자용 라이센스 키: string
+  const LICENSE_KEY: string = "dev_nacnaia9luwxmv3xqy7doo05bzpmokv4slzoy15q";
+  let started: any;
+  // GazeTracking 데이터 생성
+  const seeso: any = new Seeso();
+  
+  // seeso의 버전확인
+  const version: string = Seeso.getVersionName();
+  
+  // userStatusOption
+  const isUseAttention: boolean = true;
+  const isUseBlink: boolean = true;
+  const isUseDrowsiness: boolean = true;
+  const userStatusOption: any = new UserStatusOption(isUseAttention, isUseBlink, isUseDrowsiness);
+
+  // seeso init
+  (async () => {
+    const errorCode = seeso.initialize(
+      LICENSE_KEY, userStatusOption
+    );
+    // 트래킹시작
+    const stream: any = await navigator.mediaDevices.getUserMedia({'video': true});
+    started = await seeso.startTracking(stream);
+    // 크래킹중지
+    seeso.stopTracking()
+  })()
+  const result = seeso.startCalibration(5, 0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
